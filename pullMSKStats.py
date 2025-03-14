be4fc8ad0cee6233d12f9fbd89977101fec59830
oisraeli <oisraeli@confluent.io>
2025-03-14 03:11:40 +0200

    Update pullMSKStats.py

--- pullMSKStats.py
+++ pullMSKStats.py
@@ -1,302 +1,174 @@
 # -*- coding: utf-8 -*-
+"""
+Script to fetch AWS MSK metrics, cost data, and export them to an Excel file.
 
-# input params
-# path to the config file, see pullStatsConfig.json
-
-from optparse import OptionParser
+Usage:
+    python script.py -c pullStatsConfig.json -d output_directory
+"""
 
 import boto3
 import datetime
 import os
 import pandas as pd
 import sys
+from optparse import OptionParser
+from configparser import ConfigParser  # Works for both Python 2.7 and 3+
 
-# Metric Collection Period (in days)
+# Constants
 METRIC_COLLECTION_PERIOD_DAYS = 7
-
-# Aggregation Duration (in seconds)
 AGGREGATION_DURATION_SECONDS = 3600
 
-# Different versions of python 2.7 have renamed modules
-try:
-    from configparser import ConfigParser
-
-except ImportError:
-    from ConfigParser import ConfigParser
-
-
-def get_average_metrics():
-    metrics = [
-        'BytesInPerSec',
-        'BytesOutPerSec',
-        'MessagesInPerSec',
-        'CpuUser']
-    return metrics
-
-
-def get_peak_metrics():
-    metrics = [
-        'BytesInPerSec',
-        'BytesOutPerSec',
-        'MessagesInPerSec',
-        'CpuUser',
-        'ConnectionCount',
-        'PartitionCount',
-        'GlobalTopicCount',
-        'EstimatedMaxTimeLag',
-        'LeaderCount',
-        'ReplicationBytesOutPerSec',
-        'ReplicationBytesInPerSec',
-        'MemoryFree',
-        'MemoryUsed']
-    return metrics
-
-
-def calc_expiry_time(expiry):
-    """Calculate the number of days until the reserved instance expires.
-    Args:
-        expiry (DateTime): A timezone-aware DateTime object of the date when
-            the reserved instance will expire.
-    Returns:
-        The number of days between the expiration date and now.
-    """
-    return (expiry.replace(tzinfo=None) - datetime.datetime.utcnow()).days
+# Metrics
+AVERAGE_METRICS = ['BytesInPerSec', 'BytesOutPerSec', 'MessagesInPerSec', 'CpuUser']
+PEAK_METRICS = AVERAGE_METRICS + [
+    'ConnectionCount', 'PartitionCount', 'GlobalTopicCount', 'EstimatedMaxTimeLag',
+    'LeaderCount', 'ReplicationBytesOutPerSec', 'ReplicationBytesInPerSec',
+    'MemoryFree', 'MemoryUsed'
+]
 
 
 def get_clusters_info(session):
-    """Calculate the running/reserved instances in ElastiCache.
-    Args:
-        session (:boto3:session.Session): The authenticated boto3 session.
-    Returns:
-        A dictionary of the running/reserved instances for ElastiCache nodes.
-    """
+    """Retrieve active MSK clusters."""
     conn = session.client('kafka')
-    results = {
-        'msk_running_instances': {}
-    }
-
+    clusters = {}
     paginator = conn.get_paginator('list_clusters')
-    page_iterator = paginator.paginate()
-    # Loop through running ElastiCache instance and record their engine,
-    # type, and name.
-    for page in page_iterator:
+    for page in paginator.paginate():
         for instance in page['ClusterInfoList']:
             if instance['State'] == 'ACTIVE':
-                cluster_id = instance['ClusterName']
-                results['msk_running_instances'][cluster_id] = instance
-
-    return results
+                clusters[instance['ClusterName']] = instance
+    return {'msk_running_instances': clusters}
 
 
 def get_metric(cloud_watch, cluster_id, node, metric, is_peak, time_period=METRIC_COLLECTION_PERIOD_DAYS):
-    """Write node related metrics to file
-    Args:
-        ClusterId, node and metric to write
-    Returns:
-    The metric value
-    """
-    today = datetime.date.today() + datetime.timedelta(days=1)
-    then = today - datetime.timedelta(days=time_period)
+    """Fetch CloudWatch metrics for a given MSK broker node."""
+    end_time = datetime.date.today() + datetime.timedelta(days=1)
+    start_time = end_time - datetime.timedelta(days=time_period)
+    period = AGGREGATION_DURATION_SECONDS if is_peak else AGGREGATION_DURATION_SECONDS * 24 * time_period
 
-    period=AGGREGATION_DURATION_SECONDS
-    if not is_peak:
-        period=AGGREGATION_DURATION_SECONDS * 24 * time_period
-
+    dimensions = [{'Name': 'Cluster Name', 'Value': cluster_id}]
     if metric != 'GlobalTopicCount':
-        response = cloud_watch.get_metric_statistics(
-            Namespace='AWS/Kafka',
-            MetricName=metric,
-            Dimensions=[
-                {'Name': 'Cluster Name', 'Value': cluster_id},
-                {'Name': 'Broker ID', 'Value': str(node)}
-            ],
-            StartTime=then.isoformat(),
-            EndTime=today.isoformat(),
-            Period=period,
-            Statistics=['Average']
-        )
-    else:
-        response = cloud_watch.get_metric_statistics(
-            Namespace='AWS/Kafka',
-            MetricName=metric,
-            Dimensions=[
-                {'Name': 'Cluster Name', 'Value': cluster_id}
-            ],
-            StartTime=then.isoformat(),
-            EndTime=today.isoformat(),
-            Period=period,
-            Statistics=['Average']
-        )
+        dimensions.append({'Name': 'Broker ID', 'Value': str(node)})
 
-    rec_max = 0
-    for rec in response['Datapoints']:
-        if rec['Average'] > rec_max:
-            rec_max = rec['Average']
+    response = cloud_watch.get_metric_statistics(
+        Namespace='AWS/Kafka',
+        MetricName=metric,
+        Dimensions=dimensions,
+        StartTime=start_time.isoformat(),
+        EndTime=end_time.isoformat(),
+        Period=period,
+        Statistics=['Average']
+    )
 
-    return rec_max
+    return max((rec['Average'] for rec in response.get('Datapoints', [])), default=0)
 
 
 def create_data_frame():
-    """Create an empty dataframe with headers
-    Args:
-    Returns:
-    The newely created pandas dataframe
-    """
-    df_columns = ["Region", "ClusterName", "NodeId", "NodeType", "KafkaVersion"]
-    for metric in get_average_metrics():
-        df_columns.append(('%s (avg over last week)' % metric))
-    for metric in get_peak_metrics():
-        df_columns.append(('%s (max over last week)' % metric))
-    df = pd.DataFrame(columns=df_columns)
-    return df
+    """Create an empty DataFrame with required columns."""
+    columns = ["Region", "ClusterName", "NodeId", "NodeType", "VolumeSize (GB)", "KafkaVersion"]
+    columns += [f"{metric} (avg)" for metric in AVERAGE_METRICS]
+    columns += [f"{metric} (max)" for metric in PEAK_METRICS]
+    return pd.DataFrame(columns=columns)
 
 
-def write_cluster_info(df, clusters_info, session, region, options):
-    """Write all the data gathered to the file
-    Args:
-        The cluster information dictionary
-    Returns:
-    """
+def write_cluster_info(df, clusters_info, session, region):
+    """Populate DataFrame with MSK cluster data."""
     cloud_watch = session.client('cloudwatch')
-    row = []
-    i = 0
-
     running_instances = clusters_info['msk_running_instances']
-    for instanceId, instanceDetails in running_instances.items():
-        for nodeId in range(1, instanceDetails['NumberOfBrokerNodes'] + 1):
-            print("Getting cluster % s details" % instanceId)
-            print(instanceDetails['BrokerNodeGroupInfo'])
-            row.append("%s" % region)
-            row.append("%s" % instanceId)
-            row.append("%s" % nodeId)
-            row.append("%s" % instanceDetails['BrokerNodeGroupInfo']['InstanceType'])
-            row.append("%s" % instanceDetails['CurrentBrokerSoftwareInfo']['KafkaVersion'])
 
-            # if instanceDetails['EnhancedMonitoring'] != 'PER_TOPIC_PER_PARTITION' and \
-            #         instanceDetails['EnhancedMonitoring'] != 'PER_TOPIC' and \
-            #         instanceDetails['EnhancedMonitoring'] != 'PER_BROKER':
-            #     print("Cluster monitoring is off, cannot gather more information")
-            #     return
+    rows = []
+    for cluster_id, details in running_instances.items():
+        for node_id in range(1, details['NumberOfBrokerNodes'] + 1):
+            row = [
+                region, cluster_id, node_id,
+                details['BrokerNodeGroupInfo']['InstanceType'],
+                details['BrokerNodeGroupInfo']['StorageInfo']['EbsStorageInfo']['VolumeSize'],
+                details['CurrentBrokerSoftwareInfo']['KafkaVersion']
+            ]
 
-            for metric in get_average_metrics():
-                row.append(
-                    get_metric(
-                        cloud_watch,
-                        instanceId,
-                        nodeId,
-                        metric,
-                        False))
+            row += [get_metric(cloud_watch, cluster_id, node_id, metric, False) for metric in AVERAGE_METRICS]
+            row += [get_metric(cloud_watch, cluster_id, node_id, metric, True) for metric in PEAK_METRICS]
+            rows.append(row)
 
-            for metric in get_peak_metrics():
-                row.append(
-                    get_metric(
-                        cloud_watch,
-                        instanceId,
-                        nodeId,
-                        metric,
-                        True))
-            df.loc[i] = row
-            row = []
-            i += 1
-
-    df.sort_values(by=['ClusterName', 'NodeId'])
-
-
-def get_reserved_instances(clusters_info):
-    # create the dataframe
-    df_columns = ["Instance Type", "Count", "Remaining Time (days)"]
-    df = pd.DataFrame(columns=df_columns)
-
-    row = []
-    i = 0
-    reserved_instances = clusters_info['elc_reserved_instances']
-    for instanceId, instanceDetails in reserved_instances.items():
-        row.append(("%s" % instanceId))
-        row.append(("%s" % instanceDetails['count']))
-        row.append(("%s," % instanceDetails['expiry_time']))
-        df.loc[i] = row
-        i = i + 1
-        row = []
-
-    return (df, i)
+    return pd.DataFrame(rows, columns=df.columns)
 
 
 def get_costs(session):
-    # query pricing
-    pr = session.client('ce')
-    now = datetime.datetime.now()
-    start = (now - datetime.timedelta(days=30)).strftime('%Y-%m-%d')
-    end = now.strftime('%Y-%m-%d')
-    pricing_data = pr.get_cost_and_usage(TimePeriod={'Start': start,
-                                                     'End': end},
-                                         Granularity='MONTHLY',
-                                         Filter={
-        "And": [{"Dimensions": {'Key': 'REGION',
-                                'Values': [session.region_name]}},
-                {"Dimensions": {'Key': 'SERVICE',
-                                'Values': ['Amazon Managed Streaming for Apache Kafka']}}]},
-        Metrics=['UnblendedCost'])
+    """Fetch AWS MSK cost data from Cost Explorer."""
+    try:
+        pr = session.client('ce')
+        now = datetime.datetime.now()
+        start = (now.replace(day=1) - datetime.timedelta(days=1)).replace(day=1).strftime('%Y-%m-%d')
+        end = (now.replace(day=1) - datetime.timedelta(days=1)).strftime('%Y-%m-%d')
 
-    costs = 0
-    for res in pricing_data['ResultsByTime']:
-        costs = costs + float(res['Total']['UnblendedCost']['Amount'])
+        pricing_data = pr.get_cost_and_usage(
+            TimePeriod={'Start': start, 'End': end},
+            Granularity='MONTHLY',
+            Filter={
+                "And": [
+                    {"Dimensions": {'Key': 'REGION', 'Values': [session.region_name]}},
+                    {"Dimensions": {'Key': 'SERVICE', 'Values': ['Amazon Managed Streaming for Apache Kafka']}}
+                ]
+            },
+            Metrics=['UnblendedCost'],
+            GroupBy=[{'Type': 'DIMENSION', 'Key': 'USAGE_TYPE'}]
+        )
 
-    return costs
+        data = [
+            {"time_period": res["TimePeriod"]["Start"], "usage_type": group["Keys"][0],
+             "cost": float(group["Metrics"]["UnblendedCost"]["Amount"])}
+            for res in pricing_data['ResultsByTime'] for group in res['Groups']
+        ]
+
+        return pd.DataFrame(data)
+    except Exception as e:
+        print(f"Error querying AWS Cost Explorer: {e}")
+        return pd.DataFrame()
 
 
 def process_aws_account(config, section, options):
-    print('Grab a coffee this script takes a while...')
-    # connect to ElastiCache
-    # aws key, secret and region
-    region = config.get(section, 'region')
-    access_key = config.get(section, 'aws_access_key_id')
-    secret_key = config.get(section, 'aws_secret_access_key')
-
+    """Process MSK metrics and cost data for an AWS account."""
+    print(f'Processing AWS account: {section}')
     session = boto3.Session(
-        aws_access_key_id=access_key,
-        aws_secret_access_key=secret_key,
-        region_name=region)
+        aws_access_key_id=config.get(section, 'aws_access_key_id'),
+        aws_secret_access_key=config.get(section, 'aws_secret_access_key'),
+        region_name=config.get(section, 'region')
+    )
 
-    output_file_path = "%s/%s-%s.xlsx" % (options.outDir, section, region)
-    writer = pd.ExcelWriter(output_file_path, engine='xlsxwriter')
+    output_file = os.path.join(options.outDir, f"{section}-{session.region_name}.xlsx")
+    writer = pd.ExcelWriter(output_file, engine='xlsxwriter')
 
-    print('Writing Headers')
-    cluster_df = create_data_frame()
-
-    print('Gathering data...')
     clusters_info = get_clusters_info(session)
+    cluster_df = write_cluster_info(create_data_frame(), clusters_info, session, session.region_name)
+    cluster_df.to_excel(writer, 'ClusterData', index=False)
 
-    print('Writing data...')
-    write_cluster_info(cluster_df, clusters_info, session, region, options)
-    cluster_df.to_excel(writer, 'ClusterData')
+    costs_df = get_costs(session)
+    if not costs_df.empty:
+        costs_df = pd.concat(
+            [costs_df, pd.DataFrame([{"time_period": "TOTAL", "usage_type": "ALL", "cost": costs_df["cost"].sum()}])],
+            ignore_index=True)
+        costs_df.to_excel(writer, 'Costs', index=False)
 
-    costs = get_costs(session)
-    customer_costs = {'costs': [costs]}
-    costs_df = pd.DataFrame(customer_costs)
-    costs_df.to_excel(writer, 'Costs', index=False)
-
-    writer.save()
+    writer.close()
+    print(f'Results saved to {output_file}')
 
 
 def main():
     parser = OptionParser()
-    parser.add_option("-c", "--config", dest="configFile",
-                      help="Location of configuration file", metavar="FILE")
-    parser.add_option("-d", "--out-dir", dest="outDir", default=".",
-                      help="directory to write the results in", metavar="PATH")
+    parser.add_option("-c", "--config", dest="configFile", help="Path to configuration file", metavar="FILE")
+    parser.add_option("-d", "--out-dir", dest="outDir", default=".", help="Output directory", metavar="PATH")
+    options, _ = parser.parse_args()
 
-    (options, args) = parser.parse_args()
-    if options.configFile is None:
-        print("Please run with -h for help")
+    if not options.configFile:
+        print("Usage: script.py -c <config file> -d <output directory>")
         sys.exit(1)
 
     config = ConfigParser()
     config.read(options.configFile)
 
-    for section in config.sections():
-        if not os.path.isdir(options.outDir):
-            os.makedirs(options.outDir)
+    if not os.path.exists(options.outDir):
+        os.makedirs(options.outDir)
 
+    for section in config.sections():
         process_aws_account(config, section, options)
 
 
